b0a86da3af308413f068364e3a82caad0c2d0fe0
Soumya Vaish <vaish.1@iitj.ac.in>
2021-07-12 02:35:53 +0530

    chat working

--- node_modules/helpers.js/lib/version.js
+++
@@ -1,9 +0,0 @@
-var packageInfo = require('../package.json');
-
-module.exports = {
-  full: packageInfo.version,
-  major: packageInfo.version.split('.')[0],
-  minor: packageInfo.version.split('.')[1],
-  dot: packageInfo.version.split('.')[2],
-  author: packageInfo.author
-};
