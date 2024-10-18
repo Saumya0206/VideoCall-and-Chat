323223a38c29e0c351c6c1a8b5f7b4b4b5a9fc61
Saumya0206 <vaish.1@iitj.ac.in>
2024-10-18 16:50:13 +0530

    new changes

--- VideoCall2-Node/src/app.js
+++ VideoCall2-Node/src/app.js
@@ -6,7 +6,9 @@ let stream = require( './ws/stream' );
 let path = require( 'path' );
 let favicon = require( 'serve-favicon' );
 
-app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
+app.use( favicon( path.join( __dirname, 'favicon.ico' ) ));
+
+
 
 
 app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
